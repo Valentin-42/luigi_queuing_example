2c2047c0000b12dbb9a0bf5f0d6339a2c3dbdead
Valentin-42 <valentinvial42@gmail.com>
2024-12-04 12:02:15 -0500

    switched to logging

--- main.py
+++ main.py
@@ -2,30 +2,32 @@ import os
 import time
 import argparse
 import luigi
-import subprocess
 import datetime
+import logging
 
 # Luigi task for pipeline execution
 class RunPipelineTask(luigi.Task):
     data_path = luigi.Parameter()
 
     def run(self):
-        timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-        log_file = f"./logs/log_{timestamp}.txt"
+        timestamp = datetime.datetime.now().strftime("%Hh_%Mm_%Ss-%d_%m_%Y")
+        log_file = f"./logs/{timestamp}.log"
         
-        with open(log_file, 'w+') as f:
-            f.write('Start new task...\n')
-            f.flush()
+        # Configure the logger
+        logging.basicConfig(
+            filename=log_file,               # Specify the log file name
+            level=logging.INFO,               # Set the logging level to INFO
+            format='%(asctime)s - %(levelname)s - %(message)s'  # Define the log message format
+        )
+        
+        logging.info('Starting a new task...')
+        
+        logging.info(f"Processing data from {self.data_path}...")
 
-            # Simulate running the pipeline locally
-            f.write(f"Processing data from {self.data_path}...\n")
-            f.flush()
+        # Simulate long-running pipeline task
+        time.sleep(5)  # Simulate processing delay
 
-            # Simulate long-running pipeline task
-            time.sleep(5)  # Simulate processing delay
-
-            f.write(f"Pipeline processing complete for {self.data_path}.\n")
-            f.flush()
+        logging.info(f"Pipeline processing complete for {self.data_path}.\n")
 
     def output(self):
         return luigi.LocalTarget(f"{self.data_path}/output.txt")
